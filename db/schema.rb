43ad190fa94719d9ba826edf43762bb6fe909f1a
RicciFlowing <benjamin@mathe-sellin.de>
2015-12-10 19:25:16 +0100

    Added a skill resource

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151208185556) do
+ActiveRecord::Schema.define(version: 20151210144431) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "skills", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "skills", ["category_id"], name: "index_skills_on_category_id"
 
 end
