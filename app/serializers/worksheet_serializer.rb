d113281b21edee4c65d62d46de2055d1f319c232
RicciFlowing <benjamin@mathe-sellin.de>
2015-12-28 10:30:06 +0100

    moved depreacted serializer option in aplliction config

--- app/serializers/worksheet_serializer.rb
+++ app/serializers/worksheet_serializer.rb
@@ -2,5 +2,5 @@ class WorksheetSerializer < ActiveModel::Serializer
   attributes :id, :title
   #has_one :factory
   has_many :questions
-  embed :ids, embed_in_root: true
+  #embed :ids, embed_in_root: true
 end
--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,11 @@ module WorksheetGeneratorBackend
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    ActiveModel::Serializer.setup do |config|
+      config.embed = :ids
+      config.embed_in_root = true
+    end
+
   end
 end
