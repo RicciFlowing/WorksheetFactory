eb2f54082e2fad640b02cf649cf7161ca06a5a5e
RicciFlowing <benjamin@mathe-sellin.de>
2016-01-02 12:05:14 +0100

    Fixed many-to-many realtion between worksheets and questions

--- frontend/app/models/question.js
+++ frontend/app/models/question.js
@@ -4,5 +4,5 @@ export default DS.Model.extend({
   text: DS.attr('string'),
   solution: DS.attr('string'),
   skill: DS.belongsTo('skill'),
-  worksheet: DS.belongsTo('worksheet'),
+  worksheets: DS.hasMany('worksheet'),
 });
