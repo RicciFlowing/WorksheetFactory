0036398313b19d512e9dd9fca57da7ffccfbd4c1
RicciFlowing <benjamin@mathe-sellin.de>
2016-01-17 13:06:44 +0100

    Fixed the behavior of the worksheet form to not trigger a reload on pressing enter in an input field

--- frontend/app/controllers/groups/show.js
+++ frontend/app/controllers/groups/show.js
@@ -3,12 +3,14 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     submit(){
+       Ember.Logger.info('log value of foo:', 'foo');
       let new_worksheet = this.get('new_worksheet')
       new_worksheet.set('group', this.get('group'));
       new_worksheet.save();
       let flash = this.get('flashMessages');
       flash.success('Created a Worksheet!');
       this.send('reloadModels');
+      return false;
     }
   }
 });
