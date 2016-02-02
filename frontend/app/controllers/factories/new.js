6a03d96255d886d8855656cb6c2074d8ba818703
RicciFlowing <benjamin@mathe-sellin.de>
2016-02-02 12:48:01 +0100

    Added some styling and fixed a bug in the factory creation fomr

--- frontend/app/controllers/factories/new.js
+++ frontend/app/controllers/factories/new.js
@@ -2,21 +2,21 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   selection: Ember.A([]),
+  hasSelection: Ember.computed.alias('selection.length'),
   actions: {
     save() {
       var factory = this.get('factory')
       factory.save().then(()=>{
         this.set_skills(factory);
       });
-      this.set('selection', Ember.A([]));
       this.transitionToRoute('groups');
     },
     cancel() {
+      this.set('selection', Ember.A([]));
       this.transitionToRoute('groups');
       return false;
     },
     addToSelection(skill){
-      Ember.Logger.log(this.get('selection'));
       this.get('selection').pushObject(skill);
     },
     removeFromSelection(skill){
@@ -24,12 +24,14 @@ export default Ember.Controller.extend({
     }
   },
   set_skills(factory){
-    var skills = Ember.A(this.get('selection'));
+    let skills = this.get('selection');
     skills.forEach(
       (skill)=>{this._create_factory_skill_association(skill, factory)}
     );
+    this.set('selection', Ember.A([]));
   },
   _create_factory_skill_association(skill, factory){
+        alert('create');
     var association = this.store.createRecord('FactorySkillAssociation');
     association.set('skill', skill);
     association.set('factory', factory);
