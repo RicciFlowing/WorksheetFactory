6c6b563bd8e4e467d3ccbf60a2e72ed34c025a08
RicciFlowing <benjamin@mathe-sellin.de>
2016-02-08 15:58:34 +0100

    Removed some old and unused code from groups.show controller

--- frontend/app/controllers/groups/show.js
+++ frontend/app/controllers/groups/show.js
@@ -1,23 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  showArchive: false,
-  archivedWorksheets: Ember.computed.filterBy('group.worksheets', 'archived', true),
-  remainingWorksheets: Ember.computed.filterBy('group.worksheets', 'archived', false),
-  archiveCount: Ember.computed.alias('archivedWorksheets.length'),
-  archiveHidden:  Ember.computed.not('showArchive'),
-  boolArchiveCount: Ember.computed.bool('archiveCount'),
-  archiveButtonVisible: Ember.computed.and('boolArchiveCount', 'archiveHidden'),
-
-  actions: {
-    submit(){
-      this.send('createWorksheet', this.get('group'));
-      let flash = this.get('flashMessages');
-      flash.success('Created a Worksheet!');
-      return false;
-    },
-    toggleArchive(){
-      this.set('showArchive',!this.get('showArchive'));
-    },
-  }
 });
