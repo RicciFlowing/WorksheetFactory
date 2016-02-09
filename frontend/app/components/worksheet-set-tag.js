b35c6544c3c951a3476e7680cec0ddd9d8ea4a09
RicciFlowing <benjamin@mathe-sellin.de>
2016-02-09 09:38:00 +0100

    Added hide archive action to worksheet-set-tag

--- frontend/app/components/worksheet-set-tag.js
+++ frontend/app/components/worksheet-set-tag.js
@@ -1,11 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  showArchive: false,
   store: Ember.inject.service(),
   actions: {
     submit(){
         this.get('store').createRecord('worksheet', {worksheetSet: this.get('worksheet_set')}).save();
         return false;
         },
+    toggleArchive(){
+      this.set('showArchive', !this.get('showArchive'));
+    }
   }
 });
