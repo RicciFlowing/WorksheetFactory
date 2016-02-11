a80033dc2d4c88c94241f0d2a4020317dedb7df2
RicciFlowing <benjamin@mathe-sellin.de>
2016-02-11 18:49:11 +0100

    Moved seeds to seperate files

--- db/migrate/20151212101903_create_worksheets.rb
+++ db/migrate/20151212101903_create_worksheets.rb
@@ -2,7 +2,6 @@ class CreateWorksheets < ActiveRecord::Migration
   def change
     create_table :worksheets do |t|
       t.string :title
-      t.belongs_to :worksheet_set, index: true
 
       t.timestamps null: false
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,66 +2,8 @@ require_relative 'seeder.rb'
 
 integer = Proc.new { |min, max| rand(max-min)+min }
 
-load "seed_dir/l1.rb"
-
-## L2 Messen
-rect = Proc.new do
-  a = integer.call(3,20)
-  b = integer.call(3,20)
-  {a: a, b: b, area: a*b, umfang: 2*a+2*b}
-end
-
-Seeder.create_skill(name: 'Flächeninhalt eines Rechtecks berechnen',
-templates:
-[{template: "Berechne den Flächeninhalt eines Rechtecks mit den Seitenlängen a={{a}}cm  b={{b}}cm== {{number}}", values: rect}],
-count: 10
-)
-
-winkel = Proc.new do
-  stunden = integer.call(1,11)
-  minuten = integer.call(0,59)
-  {stunden: stunden, minuten: minuten, winkel: (stunden.to_f/12*36).round}
-end
-
-Seeder.create_skill(name: 'Winkel schätzen',
-templates:
-[{template: "Eine Uhr zeigt {{stunden}}:00.Schätze den Winkel zwischen den Zeigern == {{winkel}}", values: winkel}],
-count: 10
-)
-
-einheiten = Proc.new do
-  zahl = integer.call(1,100)
-  {zahl: zahl, hundert: zahl*100, tausend: zahl*1000}
-end
-
-Seeder.create_skill(name: 'Längeneinheiten umwandlen',
-templates:
-[
-  {template: "Wandle um {{tausend}}mm = ?m == {{zahl}} m", values: einheiten},
-  {template: "Wandle um {{tausend}}m = ?km == {{zahl}} km", values: einheiten},
-  {template: "Wandle um {{hundert}}cm = ?m == {{zahl}} m", values: einheiten},
-  {template: "Wandle um {{zahl}}m = ?mm == {{tausend}} mm", values: einheiten},
-  {template: "Wandle um {{zahl}}km = ?m == {{tausend}} m", values: einheiten},
-  {template: "Wandle um {{zahl}}m = ?cm == {{hundert}} cm", values: einheiten},
-],
-count: 20
-)
-
-Seeder.create_skill(name: 'Volumeneinheiten umwandlen',
-templates:
-[
-  {template: "Wandle um {{tausend}}ml = ?l == {{zahl}} l", values: einheiten},
-  {template: "Wandle um {{zahl}}$dm^3$ = ?l == {{zahl}} l", values: einheiten},
-  {template: "Wandle um {{zahl}}$m^3$ = ?l == {{tausend}} l", values: einheiten},
-],
-count: 20
-)
-
-## L3 Raum und Form
-
-## L4 Funktionaler Zusammenhang
-
-## L5 Daten und Zufall
+require_relative "seed_dir/l1.rb"
+require_relative "seed_dir/5_6.rb"
 
 
 shapes = Proc.new do
