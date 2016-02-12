2b253b7ecf3180198f902cee8004172bfc0335a0
RicciFlowing <benjamin@mathe-sellin.de>
2016-02-12 12:52:41 +0100

    Structed basics module

--- db/basics.rb
+++ db/basics.rb
@@ -1,15 +1,79 @@
 module Basics
   extend self
-  
-  def integer(args)
-    self.decimal(args.merge(digits: 0)).to_i
+
+
+  class Integer < ::Integer
+    def self.in_range(args)
+      max = args[:max]
+      min = args[:min]
+      result = (max-min)*rand()+min
+      result.round(0)
+    end
   end
 
-  def decimal(args)
-    max = args[:max]
-    min = args[:min]
-    digits = args[:digits]
-    result = (max-min)*rand()+min
-    result.round(digits)
+  class Float < ::Float
+    def self.in_range(args)
+      max = args[:max]
+      min = args[:min]
+      digits = args[:digits]
+      result = (max-min)*rand()+min
+      result.round(digits)
+    end
+  end
+
+  class Fraction
+    attr_reader :denominator, :numerator
+
+    def initialize(args)
+      @denominator = args[:denominator] || Basics::Integer.in_range(min: 2, max: 10)
+      @numerator   = args[:numerator] || Basics::Integer.in_range(min: 2, max: @denominator)
+    end
+
+    def to_s
+      "$\frac{#{@numerator}}{#{@denominator}}$"
+    end
+
+    def *(y)
+      Fraction.new(
+      denominator:@denominator  * y.denominator,
+      numerator: @numerator    * y.numerator
+      )
+    end
+
+    def reduce
+      factors = factors(@denominator) & factors(@numerator)
+      if factors.empty?
+        self
+      else
+        Fraction.new(denominator: @denominator/factors.max, numerator: @numerator/factors.max).reduce
+      end
+    end
+
+    private
+    def factors(number)
+      factor_list = []
+      (2..number/2).each do |possible_factor|
+        factor_list.push(possible_factor) if number.modulo(possible_factor).zero?
+      end
+      factor_list
+    end
   end
 end
+
+p 'Integer betweeb 2 and 1000'
+p Basics::Integer.in_range(min: 2, max: 1000)
+p 'Integer betweeb -2 and -1000'
+p Basics::Integer.in_range(min: -1000, max: -2)
+p 'Decimal between 2 and 5 with 3 digits'
+p Basics::Float.in_range(min: 2, max: 5, digits: 3)
+p 'Fraction'
+p Basics::Fraction.new({}).to_s
+
+frac1 = Basics::Fraction.new({})
+frac2 = Basics::Fraction.new({})
+
+p "#{frac1.to_s} * #{frac2.to_s} = #{(frac1*frac2).to_s}"
+
+frac3 =  Basics::Fraction.new(denominator: 56, numerator:42 )
+
+p "#{frac3.to_s} gekürzt= #{frac3.reduce.to_s}"
