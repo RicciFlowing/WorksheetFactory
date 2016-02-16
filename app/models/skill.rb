6104c5471ada24cf2d2cf201c49317c6a895dec8
RicciFlowing <benjamin@mathe-sellin.de>
2016-02-16 16:24:19 +0100

    Added validation for skill.name

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -4,6 +4,8 @@ class Skill < ActiveRecord::Base
   has_many :factory_skill_associations
   has_many :factories, through: :factory_skill_associations
 
+  validates :name, uniqueness: true
+
   def get_question
     self.questions.offset(rand(self.questions.count)).first
   end
