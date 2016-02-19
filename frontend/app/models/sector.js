d525eb0b140a1961b8718aa6aed5b60db325d4b9
RicciFlowing <benjamin@mathe-sellin.de>
2016-02-19 16:48:14 +0100

    Styled sector listing on factory creation page

--- frontend/app/models/sector.js
+++ frontend/app/models/sector.js
@@ -3,4 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr('string'),
   skills: DS.hasMany('skill', {async: true}),
+  skillCount: Ember.computed.alias('skills.length'),
 });
