376d8a32844cfc85466e1cb94de742a3d54b8f43
RicciFlowing <benjamin@mathe-sellin.de>
2016-02-21 11:59:08 +0100

    Added controller actions [show, create, destroy] to ResultsController

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,2 +1,32 @@
 class ResultsController < ApplicationController
+
+  before_action :set_result, only: [:show, :destroy]
+
+  def show
+    render json: @result
+  end
+
+  def create
+    @result = Result.new(result_params);
+    if @result.save
+      render json: @result, status: 200
+    else
+      render json: @result.errors, status: 400
+    end
+  end
+
+  def destroy
+    @result.destroy
+    head :no_content
+  end
+
+  private
+    def result_params
+      params.require(:result).permit(:positive, :negative, :group_id, :question_id, :worksheet_id)
+    end
+
+    def set_result
+      @result = Result.find(params[:id])
+    end
+
 end
