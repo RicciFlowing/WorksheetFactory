91af1bd9c8da35d929c48016ba7322272a25ba9f
RicciFlowing <benjamin@mathe-sellin.de>
2016-02-25 12:21:06 +0100

    Reddesigned the worksheet-ste_tag

--- frontend/app/components/worksheet-set-tag.js
+++ frontend/app/components/worksheet-set-tag.js
@@ -3,6 +3,9 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   showArchive: false,
   store: Ember.inject.service(),
+  sorting: ['id:desc'],
+  sortedRemainingWorksheets: Ember.computed.sort('worksheet_set.remainingWorksheets', 'sorting'),
+  sortedArchivedWorksheets: Ember.computed.sort('worksheet_set.archivedWorksheets', 'sorting'),
   actions: {
     submit(){
         this.get('store').createRecord('worksheet', {worksheetSet: this.get('worksheet_set')}).save();
