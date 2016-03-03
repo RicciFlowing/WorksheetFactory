9e40ee55a0cea1bfebb3fab017e3bd3604990b9b
RicciFlowing <benjamin@mathe-sellin.de>
2016-03-03 19:13:48 +0100

    Added a basic StatsticFactory

--- app/models/statistic.rb
+++ app/models/statistic.rb
@@ -1,7 +1,12 @@
-class Statistic
-  def initialize(args)
+module StatisticFactory
+  extend self
+
+  def build(args)
     @results  = args[:results]
+    Statistic.new(total_correct: self.total_correct, total_errors: self.total_errors)
   end
+
+  private
 
   def total_correct
     @results.inject {|sum, result| sum + result.positive}
@@ -11,24 +16,31 @@ class Statistic
     @results.inject {|sum, result| sum + result.negative}
   end
 
-  def top_questions(count)
-    sorted = questions_with_results.sort_by{|quest| quest[:positive]-quest[:negative]}
-    sorted.first(count)
-  end
+  # def top_questions(count)
+  #   sorted = questions_with_results.sort_by{|quest| quest[:positive]-quest[:negative]}
+  #   sorted.first(count)
+  # end
+  #
+  # def questions
+  #   @questions = @questions || @results.map {|result| result.question}.uniq
+  # end
+  #
+  # def questions_with_results
+  #   unless @questions_with_results
+  #     @questions_with_results = []
+  #     questions.each do |question|
+  #       pos = question.results.inject(0){|sum, result| sum+result.positive.to_i}
+  #       neg = question.results.inject(0){|sum, result| sum+result.negative.to_i}
+  #       @questions_with_results.push({text: question.text, solution: question.solution, negative: neg, positive: pos})
+  #     end
+  #   end
+  #   @questions_with_results
+  # end
 
-  def questions
-    @questions = @questions || @results.map {|result| result.question}.uniq
-  end
+end
 
-  def questions_with_results
-    unless @questions_with_results
-      @questions_with_results = []
-      questions.each do |question|
-        pos = question.results.inject(0){|sum, result| sum+result.positive.to_i}
-        neg = question.results.inject(0){|sum, result| sum+result.negative.to_i}
-        @questions_with_results.push({question: question, negative: neg, positive: pos})
-      end
-    end
-    @questions_with_results
-  end
+class Statistic
+  include ActiveAttr::Attributes
+  attribute :total_correct
+  attribute :total_errors
 end
