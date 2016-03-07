5eb71f0e8082efb11c6be72f092c72ad22907944
RicciFlowing <benjamin@mathe-sellin.de>
2016-03-07 16:19:07 +0100

    Added Sessions to backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'sessions' }
   mount_ember_app :frontend, to: "/"
   resources :results
   resources :sectors
